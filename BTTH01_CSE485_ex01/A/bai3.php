d37115c537a631f3ad138231a69ea582ef48e370
nguyenhonganh-TLU <honganhnguyen2212@gmail.com>
2023-09-19 08:20:02 +0700

    Submit(1)

--- BTTH01_CSE485_ex01/A/bai3.php
+++
@@ -1,9 +0,0 @@
-<?php
-$arrs = ['PHP', 'HTML', 'CSS', 'JS'];
-
-echo "<table border='1'>";
-foreach ($arrs as $course) {
-    echo "<tr><td>Tên khóa học</td><td>" . $course . "</td></tr>";
-}
-echo "</table>";
-?>
